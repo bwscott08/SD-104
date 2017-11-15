37e72e173661de6fa084d2192ac4021f46ecbbec
Byron Scott <bwscott08@hotmaill.com>
2017-11-14 18:37:58 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿/*Project: Lab 2 Batting Average
+ *   Date: 11-13-2017
+ * Author: B.Scott
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_batting_average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            //variable declaration
+            string strPlayerName;
+            int nHits, nAtBats;
+            double dblBattingAverage;
+
+            //prompt user
+            Console.WriteLine("---Calculating Batting Average---");
+            Console.WriteLine("Enter the player's name: ");
+            strPlayerName = Console.ReadLine();
+            
+            //prompt user for hits
+            Console.WriteLine("Enter the player's # of hits: ");
+            nHits = Convert.ToInt32 (Console.ReadLine());
+
+            //prompt user for at bats
+            Console.WriteLine("Enter the player's # of at bats: ");
+            nAtBats = Convert.ToInt32(Console.ReadLine());
+
+            //calculation
+            dblBattingAverage = (double)nHits / nAtBats;
+
+            //display results
+            Console.WriteLine(strPlayerName + "'s batting average is " + dblBattingAverage);
+            Console.ReadLine();
+
+        }
+    }
+}
